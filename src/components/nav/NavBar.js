c76d12e6deaee1bcff232db6b1465ca13b42fa6b
Robert Davis <rkdavis15@gmail.com>
2021-09-20 15:40:32 -0400

    Messing with MUI

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,33 +1,26 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
+import Button from "@mui/material/Button";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import Toolbar from "@mui/material/Toolbar";
+
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
     <Fragment>
-      <nav className={styles["nav-bar"]}>
-        <ul>
-          <li>
-            <Link to="/">Logo</Link>
-          </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-        </ul>
-      </nav>
+      <Toolbar className={styles["nav-bar"]}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/cart">Cart</Link>
+        <Button variant="contained" endIcon={<ShoppingCartIcon />}>
+  Cart
+</Button>
+      </Toolbar>
     </Fragment>
   );
 };
